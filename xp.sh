c2f8bda9dc283070b64fa831562a1a9f2946acd5
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-01-18 13:57:04 +0700

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition V1.0
-# Auther  : Geo Project
-# (C) Copyright 2022
-# =========================================
 clear
 #----- Auto Remove Vmess
 data=( `cat /usr/local/etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
@@ -74,21 +69,5 @@ if [[ "$exp2" -le "0" ]]; then
 sed -i "/^### $user $exp/,/^},{/d" /usr/local/etc/xray/trojan.json
 rm -f /home/vps/public_html/$user-TRTCP.yaml
 systemctl restart xray@trojan.service
-fi
-done
-
-#----- Auto Remove Trojan TCP XTLS
-data=( `cat /usr/local/etc/xray/xtrojan.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/usr/local/etc/xray/xtrojan.json" | cut -d ' ' -f 3 | sort | uniq)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" -le "0" ]]; then
-sed -i "/^### $user $exp/,/^},{/d" /usr/local/etc/xray/xtrojan.json
-rm -f /home/vps/public_html/$user-TRDIRECT.yaml /home/vps/public_html/$user-TRSPLICE.yaml
-systemctl restart xray@xtrojan.service
 fi
 done
