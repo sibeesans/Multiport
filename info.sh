2e2db802bdaac52c438ee2d01bf1948390b80019
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-01-18 18:03:32 +0700

    Update info.sh

--- info.sh
+++ info.sh
@@ -14,7 +14,7 @@ WB='\e[37;1m'
 
 echo ""
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "      ${WB}Multiport Websocket Autoscript By wunuit${NC}"
+echo -e "      ${WB}Xray Websocket Autoscript By BagoesVpn${NC}"
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e "  ${WB}»»» Protocol Service «««  |  »»» Network Protocol «««${NC}  "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
