8239fb625ccc251cce27ced033be1d25b1e1086c
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-01-18 12:12:48 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition V1.0
-# Auther  : Geo Project
-# (C) Copyright 2022
-# =========================================
 red='\e[1;31m'
 green='\e[0;32m'
 purple='\e[0;35m'
@@ -22,9 +17,9 @@ unzip -P $InputPass /root/backup.zip &> /dev/null
 rm -f backup.zip
 sleep 1
 echo -e "[ ${green}INFO${NC} ] Start Restore . . . "
-#cp -r /root/backup/.acme.sh /root/ &> /dev/null
-#cp -r /root/backup/premium-script /var/lib/ &> /dev/null
-#cp -r /root/backup/xray /usr/local/etc/ &> /dev/null
+cp -r /root/backup/.acme.sh /root/ &> /dev/null
+cp -r /root/backup/premium-script /var/lib/ &> /dev/null
+cp -r /root/backup/xray /usr/local/etc/ &> /dev/null
 cp -r /root/backup/*.json /etc/xray/ >/dev/null
 cp -r /root/backup/public_html /home/vps/ &> /dev/null
 cp -r /root/backup/crontab /etc/ &> /dev/null
