a6d0f221601a93df6b73bb2f2854efd880eeed4e
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-01-19 06:33:24 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -65,4 +65,9 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      --data '{"type":"A","name":"'${WILD_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Host : $SUB_DOMAIN"
 echo $SUB_DOMAIN > /root/domain
+cp /root/domain /etc/xray/
+echo $SUB_DOMAIN > /etc/v2ray/domain
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /root/domain
+echo $SUB_DOMAIN > /etc/xray/domain
 rm -f /root/cf.sh
